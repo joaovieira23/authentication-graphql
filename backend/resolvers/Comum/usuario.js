8e1095d36ebaed85456ac341573f1cdee96f3849
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-21 16:43:20 -0300

    feat: add user login

---
+++ backend/resolvers/Comum/usuario.js
@@ -0,0 +1,24 @@
+const jwt = require('jwt-simple')
+const { perfis: obterPerfis } = require('../Type/Usuario')
+
+module.exports = {
+  async getUsuarioLogado(usuario) {
+    const perfis = await obterPerfis(usuario)
+    const atual = Math.floor(Date.now() / 1000)
+
+    const usuarioInfo = {
+      id: usuario.id,
+      nome: usuario.nome,
+      email: usuario.email,
+      perfis: perfis.map(p => p.nome),
+      iat: atual,
+      exp: atual + (3 * 24 * 60 * 60)
+    }
+
+    const authSecret = process.env.APP_AUTH_SECRET
+    return {
+      ...usuarioInfo,
+      token: jwt.encode(usuarioInfo, authSecret)
+    }
+  }
+}
--- backend/resolvers/Query/usuario.js
+++ backend/resolvers/Query/usuario.js
@@ -1,6 +1,30 @@
 const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const { getUsuarioLogado } = require('../Comum/usuario');
 
 module.exports = {
+
+    async login(_, { dados }) {
+        const usuario = await db('usuarios')
+            .where({ email: dados.email })
+            .first()
+        
+        if(!usuario) {
+            throw new Error('Usuário/Senha inválido')
+        }
+
+        const comparaSenha = bcrypt.compareSync(dados.senha,
+            usuario.senha)
+        
+        if(!comparaSenha) {
+            throw new Error('Usuário/Senha inválidos')
+        }
+
+        return getUsuarioLogado(usuario)
+
+
+    },
+
     usuarios() {
         return db('usuarios')
     },
