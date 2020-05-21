469a27b484b13dd5c0a85d752a254621e2871472
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-21 15:56:03 -0300

    feat: register user

--- backend/resolvers/Mutation/usuario.js
+++ backend/resolvers/Mutation/usuario.js
@@ -1,19 +1,40 @@
+const bcrypt = require('bcrypt-nodejs')
 const db = require('../../config/db')
 const { perfil: obterPerfil } = require('../Query/perfil')
 const { usuario: obterUsuario } = require('../Query/usuario')
 
-module.exports = {
+const mutations = {
+    registrarUsuario(_, { dados }) {
+        return mutations.novoUsuario(_, {
+            dados: {
+                nome: dados.nome,
+                email: dados.email,
+                senha: dados.senha,
+            }
+        })
+    },
+
     async novoUsuario(_, { dados }) {
         try {
             const idsPerfis = []
-            if(dados.perfis) {
-                for(let filtro of dados.perfis) {
-                    const perfil = await obterPerfil(_, {
-                        filtro
-                    })
-                    if(perfil) idsPerfis.push(perfil.id)
-                }
+
+            if(!dados.perfis || !dados.perfis.length) {
+                dados.perfis = [{
+                    nome: 'comum'
+                }]
             }
+
+            for(let filtro of dados.perfis) {
+                const perfil = await obterPerfil(_, {
+                    filtro
+                })
+                if(perfil) idsPerfis.push(perfil.id)
+            }
+
+            //criptografar senha
+            const salt = bcrypt.genSaltSync()
+            dados.senha = bcrypt.hashSync(dados.senha, salt)
+            
 
             delete dados.perfis
 
@@ -64,6 +85,13 @@ module.exports = {
                         })
                     }
                 }
+
+                if(dados.senha) {
+                    //criptografar senha
+                    const salt = bcrypt.genSaltSync()
+                    dados.senha = bcrypt.hashSync(dados.senha, salt)
+                }
+
                 delete dados.perfis
                 await db("usuarios").where({ id }).update(dados)
 
@@ -74,3 +102,5 @@ module.exports = {
         }
     }
 }
+
+module.exports = mutations
